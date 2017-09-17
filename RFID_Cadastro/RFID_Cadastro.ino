29f12dc668841e450a07de821b239d341bce5653
Lucas Osse <lucasosse7@gmail.com>
2017-09-17 22:51:50 +0000

    added Arduino folders

---
+++ RFID_Cadastro/RFID_Cadastro.ino
@@ -0,0 +1,70 @@
+// Programa: CONTROLE DE ACESSO VIA RFID. 
+ 
+//Bibliotecas
+#include <SPI.h>
+#include <MFRC522.h>
+ 
+//Pinos
+#define SS_PIN 10
+#define RST_PIN 9
+ 
+String IDtag = ""; //Variável que armazenará o ID da Tag
+bool Permitido = false; //Variável que verifica a permissão 
+ 
+//Vetor responsável por armazenar os ID's das Tag's cadastradas
+String TagsCadastradas[] = {"ID_1", 
+                            "ID_2",
+                            "ID_3"};
+ 
+MFRC522 LeitorRFID(SS_PIN, RST_PIN);    // Cria uma nova instância para o leitor e passa os pinos como parâmetro
+ 
+ 
+void setup() {
+        Serial.begin(9600);             // Inicializa a comunicação Serial
+        SPI.begin();                    // Inicializa comunicacao SPI 
+        LeitorRFID.PCD_Init();          // Inicializa o leitor RFID
+}
+ 
+void loop() {  
+  Leitura();  //Chama a função responsável por fazer a leitura das Tag's
+}
+ 
+void Leitura(){
+ 
+        IDtag = ""; //Inicialmente IDtag deve estar vazia.
+        
+        // Verifica se existe uma Tag presente
+        if ( !LeitorRFID.PICC_IsNewCardPresent() || !LeitorRFID.PICC_ReadCardSerial() ) {
+            delay(50);
+            return;
+        }
+        
+        // Pega o ID da Tag através da função LeitorRFID.uid e Armazena o ID na variável IDtag        
+        for (byte i = 0; i < LeitorRFID.uid.size; i++) {        
+            IDtag.concat(String(LeitorRFID.uid.uidByte[i], HEX));
+        }        
+        
+        //Compara o valor do ID lido com os IDs armazenados no vetor TagsCadastradas[]
+        for (int i = 0; i < (sizeof(TagsCadastradas)/sizeof(String)); i++) {
+          if(  IDtag.equalsIgnoreCase(TagsCadastradas[i])  ){
+              Permitido = true; //Variável Permitido assume valor verdadeiro caso o ID Lido esteja cadastrado
+          }
+        }       
+ 
+        if(Permitido == true) acessoLiberado(); //Se a variável Permitido for verdadeira será chamada a função acessoLiberado()        
+        else acessoNegado(); //Se não será chamada a função acessoNegado()
+ 
+        delay(2000); //aguarda 2 segundos para efetuar uma nova leitura
+}
+ 
+void acessoLiberado(){
+  Serial.println("Tag Cadastrada: " + IDtag); //Exibe a mensagem "Tag Cadastrada" e o ID da tag não cadastrada
+    Permitido = false;  //Seta a variável Permitido como false novamente
+}
+ 
+void acessoNegado(){
+  Serial.println("Tag NAO Cadastrada: " + IDtag); //Exibe a mensagem "Tag NAO Cadastrada" e o ID da tag cadastrada
+  
+}
+ 
+
---
+++ Tela_16x2_I2C/Tela_16x2_I2C.ino
@@ -0,0 +1,24 @@
+// Programa : Display LCD 16x2 e modulo I2C
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// Inicializa o display no endereco 0x27 ou 0x3f
+LiquidCrystal_I2C lcd(0x3f,2,1,0,4,5,6,7,3, POSITIVE);
+ 
+void setup()
+{
+ lcd.begin (16,2);
+}
+ 
+void loop()
+{
+  lcd.setBacklight(HIGH);
+  lcd.setCursor(0,0);
+  lcd.print("Foodtece!!!");
+  lcd.setCursor(0,1);
+  lcd.print("Sejam Bem-vindo");
+  delay(4000);
+  lcd.setBacklight(LOW);
+  delay(1000);
+}
